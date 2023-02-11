3b8a70be413901adf8e10727d3f092015a1ad448
Nadia Slavkova <113839590+gumi4kata@users.noreply.github.com>
2023-02-11 23:43:58 +0200

    Update 3-sumFirstLast.js

--- 02-Arrays and Nested Arrays/lab/3-sumFirstLast.js
+++ 02-Arrays and Nested Arrays/lab/3-sumFirstLast.js
@@ -2,3 +2,16 @@ function solve(arr) {
     arr = arr.map(Number)
     return arr[0] + arr[arr.length - 1]
 }
+
+// Other solution
+// function solve(strings) {
+
+//     let arr = strings.map(Number);
+
+//     let first = arr.shift();
+//     let last = arr.pop();
+
+//     return first + last;
+
+// }
+// console.log(solve(['20', '30', '40']));
